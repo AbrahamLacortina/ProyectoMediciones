a509a9452961751f26df3a6a9fab768b43518249
Jose <J.cornejo37@alumnos.santotomas.cl>
2025-06-12 22:11:42 -0400

    Docker y bd Actualizadas

--- mysql-scripts-init/monitoreo_ambiental_estaciones.sql
+++ mysql-scripts-init/monitoreo_ambiental_estaciones.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `monitoreo_ambiental` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `monitoreo_ambiental`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: monitoreo_ambiental
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-10 16:02:36
+-- Dump completed on 2025-06-12 20:27:17
